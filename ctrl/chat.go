cb5634147adf39a6f37e90436555c6cd75fc53a7
goular <zhaojt_cs@126.com>
2019-06-05 15:22:44 +0800

    添加分布式的UDP广播机制的代码

--- ctrl/chat.go
+++ ctrl/chat.go
@@ -2,10 +2,10 @@ package ctrl
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/websocket"
 	"gopkg.in/fatih/set.v0"
 	"log"
+	"net"
 	"net/http"
 	"strconv"
 	"sync"
@@ -76,7 +76,7 @@ var rwlocker sync.RWMutex
 // ws://127.0.0.1/chat?id=1&token=xxxx
 func Chat(writer http.ResponseWriter,
 	request *http.Request) {
-
+	//fmt.Printf("%+v",request.Header)
 	//todo 检验接入是否合法
 	//checkToken(userId int64,token string)
 	query := request.URL.Query()
@@ -115,22 +115,8 @@ func Chat(writer http.ResponseWriter,
 	go sendproc(node)
 	//todo 完成接收逻辑
 	go recvproc(node)
-	//
+	log.Printf("<-%d\n", userId)
 	sendMsg(userId, []byte("hello,world!"))
-}
-
-//发送协程
-func sendproc(node *Node) {
-	for {
-		select {
-		case data := <-node.DataQueue:
-			err := node.Conn.WriteMessage(websocket.TextMessage, data)
-			if err != nil {
-				log.Println(err.Error())
-				return
-			}
-		}
-	}
 }
 
 //todo 添加新的群ID到用户的groupset中
@@ -146,7 +132,21 @@ func AddGroupId(userId, gid int64) {
 	//添加gid到set
 }
 
-//接收协程
+//ws发送协程
+func sendproc(node *Node) {
+	for {
+		select {
+		case data := <-node.DataQueue:
+			err := node.Conn.WriteMessage(websocket.TextMessage, data)
+			if err != nil {
+				log.Println(err.Error())
+				return
+			}
+		}
+	}
+}
+
+//ws接收协程
 func recvproc(node *Node) {
 	for {
 		_, data, err := node.Conn.ReadMessage()
@@ -154,18 +154,84 @@ func recvproc(node *Node) {
 			log.Println(err.Error())
 			return
 		}
-		//todo 对data进一步处理
-		dispatch(data)
-		fmt.Printf("recv<=%s", data)
+		//dispatch(data)
+		//把消息广播到局域网
+		broadMsg(data)
+		log.Printf("[ws]<=%s\n", data)
 	}
+}
+
+func init() {
+	go udpsendproc()
+	go udprecvproc()
+}
+
+//用来存放发送的要广播的数据
+var udpsendchan chan []byte = make(chan []byte, 1024)
+
+//todo 将消息广播到局域网
+func broadMsg(data []byte) {
+	udpsendchan <- data
+}
+
+//todo 完成udp数据的发送协程
+func udpsendproc() {
+	log.Println("start udpsendproc")
+	//todo 使用udp协议拨号
+	con, err := net.DialUDP("udp", nil,
+		&net.UDPAddr{
+			IP:   net.IPv4(192, 168, 0, 255),
+			Port: 3000,
+		})
+	defer con.Close()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	//todo 通过的到的con发送消息
+	//con.Write()
+	for {
+		select {
+		case data := <-udpsendchan:
+			_, err = con.Write(data)
+			if err != nil {
+				log.Println(err.Error())
+				return
+			}
+		}
+	}
+}
+
+//todo 完成upd接收并处理功能
+func udprecvproc() {
+	log.Println("start udprecvproc")
+	//todo 监听udp广播端口
+	con, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IPv4zero,
+		Port: 3000,
+	})
+	defer con.Close()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	//TODO 处理端口发过来的数据
+	for {
+		var buf [512]byte
+		n, err := con.Read(buf[0:])
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		//直接数据处理
+		dispatch(buf[0:n])
+	}
+	log.Println("stop updrecvproc")
 }
 
 //后端调度逻辑处理
 func dispatch(data []byte) {
 	//todo 解析data为message
 	msg := Message{}
-	// todo:打印发送过来的内容
-	fmt.Println(string(data))
 	err := json.Unmarshal(data, &msg)
 	if err != nil {
 		log.Println(err.Error())
@@ -179,7 +245,6 @@ func dispatch(data []byte) {
 		//todo 群聊转发逻辑
 		for _, v := range clientMap {
 			if v.GroupSets.Has(msg.Dstid) {
-				fmt.Println("<=send", msg)
 				v.DataQueue <- data
 			}
 		}
